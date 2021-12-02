70bb7cb0005e9320385d2724bde6983e88c54bbc
aleksandrbarinov <barabarinov@gmail.com>
2021-12-03 00:43:01 +0200

    bugs fixed

--- app/handlers/report_of_all_purchase_categories.py
+++ app/handlers/report_of_all_purchase_categories.py
@@ -12,7 +12,6 @@ logger = logging.getLogger(__name__)
 def get_sum_of_all_purchases_categories(update: Update, context: CallbackContext):
     with Session() as session:
         user = session.query(User).get(update.effective_user.id)
-        result = 0
         for group in user.groups:
             logger.info(f'HERE IS GROUP {group}')
             result = sum(purchase.spent_money for purchase in group.purchases)
