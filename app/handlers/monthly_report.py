915d3f4235a472b1839b74e137584baf1cb0a105
aleksandrbarinov <barabarinov@gmail.com>
2021-12-17 17:37:35 +0200

    fixed time in purchase, incomes reports

--- app/handlers/monthly_report.py
+++ app/handlers/monthly_report.py
@@ -14,8 +14,8 @@ def get_monthly_report_start_end(user):
     now = datetime.datetime.now()
     _, days_in_previous_month = monthrange(now.year, now.month - 1)
     if now.month == 1:
-        start = datetime.datetime(now.year - 1, month=12, day=1)
-        end = datetime.datetime(now.year - 1, month=12, day=31)
+        start = datetime.datetime(now.year - 1, month=12, day=1, hour=00, minute=00, second=00)
+        end = datetime.datetime(now.year - 1, month=12, day=31, hour=23, minute=59, second=59)
     else:
         start = datetime.datetime(now.year, now.month - 1, 1)
         end = datetime.datetime(now.year, now.month - 1, days_in_previous_month)
