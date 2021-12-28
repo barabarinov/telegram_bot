03c8780769b4caff62e3c4f6e5d7d5ebc83106c9
aleksandrbarinov <barabarinov@gmail.com>
2021-12-28 11:39:34 +0200

    fixed func delete  telegram id

--- app/handlers/delete.py
+++ app/handlers/delete.py
@@ -6,9 +6,11 @@ from app.models import User
 
 
 def delete_my_telegram_id_from_telegram_bot(update: Update, context: CallbackContext):
+    telegram_id = update.effective_user.id
     with Session() as session:
+        telegram_id = session.query(User).get(telegram_id)
         user = User(
-            telegram_id=update.effective_user.id,
+            telegram_id=telegram_id,
         )
         session.delete(user)
         session.commit()
